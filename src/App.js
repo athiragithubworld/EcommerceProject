9152b1de74467f093d3ebd762abbca46b483d04a
athiragithubworld <athirahrms@gmail.com>
2023-04-22 00:10:47 +0530

    App js file commited

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Ecommerce from "./Components/Ecommerce";
+import { Button } from "react-bootstrap";
+// import ProductList from "./Components/ProductList";
+function App() {
+  return (
+    <>
+      <Ecommerce></Ecommerce>
+      {/* <ProductList></ProductList> */}
+      
+     
+    </>
+  );
+}
+
+export default App;
