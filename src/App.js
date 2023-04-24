02064f87bf3ed0e82c882843e46466ab172e993e
athiragithubworld <athirahrms@gmail.com>
2023-04-24 18:45:14 +0530

    Some changes in App js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
+import React , {useState} from "react";
+
 import Ecommerce from "./Components/Ecommerce";
 import { Button } from "react-bootstrap";
+import Cart from "./Components/Cart/Cart";
+
+
 // import ProductList from "./Components/ProductList";
+
+
 function App() {
+  
+  const [openCart ,setOpenCart] = useState(false)
+
+  const clickOpenCart = () =>{
+     setOpenCart(true)
+    }
+
+  const closeCart = () =>{
+    setOpenCart(false)
+    }
+
   return (
     <>
-      <Ecommerce></Ecommerce>
-      {/* <ProductList></ProductList> */}
+
+      <Ecommerce onClick={clickOpenCart}></Ecommerce>
+      {openCart && <Cart onClose={closeCart}></Cart>}
       
      
     </>
