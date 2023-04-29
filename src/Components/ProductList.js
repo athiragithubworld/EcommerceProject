b789674b28668bdb34c1023db9d27636a07799ca
athiragithubworld <athirahrms@gmail.com>
2023-04-29 22:48:35 +0530

    ProductList.js(Add Alertmessage)

--- src/Components/ProductList.js
+++ src/Components/ProductList.js
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import classes from "./ProductList.module.css";
-import { Figure, Button, Container, Row, Col } from "react-bootstrap";
+import { Figure, Button } from "react-bootstrap";
+import CartContext from "./store/CartContext";
+import AlertMessage from "./UI/AlertMessage";
 
 const ProductList = () => {
+  const cartcxt = useContext(CartContext);
+  const [name, setName] = useState("");
+  const [alert, setAlert] = useState(false);
+
   const productsArr = [
     {
       id: 1,
@@ -40,46 +46,67 @@ const ProductList = () => {
         "https://prasadyash2411.github.io/ecom-website/img/Album%204.png",
     },
   ];
+  
+
+  useEffect(() => {
+    setTimeout(() => {
+      setAlert(false);
+    }, 3000);
+  }, [name]);
+
+  const addItemTocart = (item) => {
+    
+      if (name !== item.name) {
+        setAlert(true);
+        setName(item.name);
+      }
+    
+
+    
+    cartcxt.addProduct({ ...item, quantity: 1 });
+  };
 
   return (
     <div>
       {productsArr.map((item) => {
+        
         return (
           // <Container fluid="md" className={classes.container}>
-            
-              
-            <Figure >
+          <>
+            <Figure>
               <Figure.Caption className={classes.caption}>
                 <h3>{item.name}</h3>
               </Figure.Caption>
 
-              <div  className={classes.container}>
-              <Figure.Image
-                
-                width={250}
-                height={250}
-                alt="250x250"
-                
-                src={item.imageUrl}
-              />
+              <div className={classes.container}>
+                <Figure.Image
+                  width={250}
+                  height={250}
+                  alt="250x250"
+                  src={item.imageUrl}
+                />
               </div>
               <Figure.Caption>
                 <span className={classes.price}>$ {item.price}</span>
                 <span>
-                  <Button variant="info"
+                  <Button
+                    variant="info"
                     style={{
                       color: "white",
                       fontWeight: "bold",
                       borderRadius: "0px",
-                      marginTop:"10px"
+                      marginTop: "10px",
                     }}
+                    onClick={() => addItemTocart(item)}
                   >
                     ADD TO CART
                   </Button>
                 </span>
               </Figure.Caption>
             </Figure>
-          
+
+            {alert && <AlertMessage name={name} />}
+          </>
         );
       })}
     </div>
