b5d34ab8ae102a60eb096eaad00ed83f9d7ffa8f
athiragithubworld <athirahrms@gmail.com>
2023-05-20 08:59:38 +0530

    FooterNavBar js file some update

--- src/Components/UI/FooterNavBar.js
+++ src/Components/UI/FooterNavBar.js
@@ -1,12 +1,11 @@
 import React from "react";
-import { Navbar, Button, Nav , Container} from "react-bootstrap";
+import { Navbar, Button, Nav, Container } from "react-bootstrap";
+import NavbarCollapse from "react-bootstrap/esm/NavbarCollapse";
 
-const FooterNavBar = () =>{
-
-
-    return(
-        <>
-        <Navbar
+const FooterNavBar = () => {
+  return (
+    <>
+      <Navbar
         variant="info"
         bg="info"
         // expand="lg"
@@ -15,60 +14,52 @@ const FooterNavBar = () =>{
           fontSize: "40px",
           fontWeight: "bold",
           fontFamily: "Times New Roman, Times, serif ",
-          paddingLeft: "100px",
+          // paddingLeft: "100px",
           position: "bottom",
           marginTop: "200px",
           textAlign: "center",
-          width:"100vw"
+          width: "100%",
         }}
       >
-        <Nav> The Generics </Nav>
+        <Container style={{ width: "70%" }}>
+          <Nav> The Generics </Nav>
 
-        <Nav>
-          <Container
-            style={{
-             
-              display: "flex",
-              // alignItems: "center ",
-              // marginLeft: "800px",
-              // //  marginInline:"100px"
-              position:"relative",
-              right:"-300px",
-              bottom:"1px"
-            }}
-          >
-            <Nav.Link href="https://www.youtube.com/">
-              <img
-                
-                width={30}
-                height={26}
-                alt="30x26"
-                src="https://prasadyash2411.github.io/ecom-website/img/6260efc8fc9a9002669d2f4ad9956cc0.jpg"
-              ></img>
-            </Nav.Link>
-
-            <Nav.Link href="https://spotify.com/">
-              <img
-                width={30}
-                height={26}
-                alt="30x26"
-                src="https://prasadyash2411.github.io/ecom-website/img/Spotify Logo.png"
-              ></img>
-            </Nav.Link>
-
-            <Nav.Link href="https://facebook.com/">
-              <img
-                width={30}
-                height={26}
-                alt="30x26"
-                src="https://prasadyash2411.github.io/ecom-website/img/Facebook Logo.png"
-              ></img>
-            </Nav.Link>
-          </Container>
-        </Nav>
+          <Nav>
+            <Navbar.Collapse>
+              <Nav.Link href="https://www.youtube.com/">
+                <img
+                  width={30}
+                  height={26}
+                  alt="30x26"
+                  src="https://prasadyash2411.github.io/ecom-website/img/6260efc8fc9a9002669d2f4ad9956cc0.jpg"
+                ></img>
+              </Nav.Link>
+            </Navbar.Collapse>
+            <Navbar.Collapse>
+              <Nav.Link href="https://spotify.com/">
+                <img
+                  width={30}
+                  height={26}
+                  alt="30x26"
+                  src="https://prasadyash2411.github.io/ecom-website/img/Spotify Logo.png"
+                ></img>
+              </Nav.Link>
+            </Navbar.Collapse>
+            <Navbar.Collapse>
+              <Nav.Link href="https://facebook.com/">
+                <img
+                  width={30}
+                  height={26}
+                  alt="30x26"
+                  src="https://prasadyash2411.github.io/ecom-website/img/Facebook Logo.png"
+                ></img>
+              </Nav.Link>
+            </Navbar.Collapse>
+          </Nav>
+        </Container>
       </Navbar>
-        </>
-    )
-}
+    </>
+  );
+};
 
 export default FooterNavBar;
