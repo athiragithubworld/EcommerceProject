002ef2e20239b2cb379be86e85bfc9fa72525a1e
athiragithubworld <athirahrms@gmail.com>
2023-05-20 08:57:06 +0530

    ProductStore js file (some update)

--- src/Components/Pages/ProductStore.js
+++ src/Components/Pages/ProductStore.js
@@ -1,30 +1,33 @@
-import React, { useContext , useState,useEffect } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { Button, Container } from "react-bootstrap";
 import { useLocation } from "react-router-dom";
 import ProductList from "../ProductList";
-import classes from './ProductStore.module.css'
+import classes from "./ProductStore.module.css";
 import CartContext from "../store/CartContext";
 
-const ProductStore = (props) =>{
-  const location=useLocation()
+const ProductStore = (props) => {
+  const location = useLocation();
 
-  const [hideButton ,setHideButton] = useState(false)
-  
-  useEffect(() =>{
-    if(location.pathname==="/"){
-        setHideButton(true)
-      }
-      else{
-        setHideButton(false)
-      }
-  },[location.pathname])
-    const cartcntxt = useContext(CartContext)
-    
+  const [hideButton, setHideButton] = useState(false);
 
-    return(
-        <>
-        
-         <Container>
+  useEffect(() => {
+    if (location.pathname === "/") {
+      setHideButton(true);
+    } else {
+      setHideButton(false);
+    }
+  }, [location.pathname]);
+  const cartcntxt = useContext(CartContext);
+
+  return (
+    <>
+      <Container
+      // style={{
+      //   maxWidth: "900px",
+      //   margin: "0 auto",
+      //   padding: "20px 30px",
+      // }}
+      >
         <h2
           style={{
             fontFamily: "MetalMania",
@@ -33,36 +36,38 @@ const ProductStore = (props) =>{
             textAlign: "center",
           }}
         >
-          
           MUSIC
         </h2>
-       
+
         <ProductList></ProductList>
-        
-        <div>
-        {hideButton && <Button
-          variant="secondary"
+
+        <div
           style={{
-            marginTop: "200px",
-            textAlign: "center",
-            marginLeft: "680px",
-            fontSize: "15px",
-            fontWeight: "bold",
-            padding: "10px",
-            color: "rgb(0, 200, 255)",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
           }}
-          // className={classes.seeCartButton}
-         
-          onClick={props.onClicks}
         >
-          
-          See the Cart
-        </Button>}
-      </div>
+          {hideButton && (
+            <Button
+              variant="secondary"
+              style={{
+                marginTop: "100px",
+
+                fontSize: "15px",
+                fontWeight: "bold",
+
+                color: "rgb(0, 200, 255)",
+              }}
+              onClick={props.onClicks}
+            >
+              See the Cart
+            </Button>
+          )}
+        </div>
       </Container>
-     
-        </>
-    )
-}
+    </>
+  );
+};
 
 export default ProductStore;
