d464f955adea4bb765c06892d1606ce9f0b6e54a
athiragithubworld <athirahrms@gmail.com>
2023-05-20 08:56:23 +0530

    ProductList js ( Some update)

--- src/Components/ProductList.js
+++ src/Components/ProductList.js
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from "react";
 import classes from "./ProductList.module.css";
-import { Figure, Button } from "react-bootstrap";
+import { Figure, Button, Container, Row, Col } from "react-bootstrap";
 import CartContext from "./store/CartContext";
 import AlertMessage from "./UI/AlertMessage";
 
@@ -46,7 +46,6 @@ const ProductList = () => {
         "https://prasadyash2411.github.io/ecom-website/img/Album%204.png",
     },
   ];
-  
 
   useEffect(() => {
     setTimeout(() => {
@@ -55,62 +54,121 @@ const ProductList = () => {
   }, [name]);
 
   const addItemTocart = (item) => {
-    
-      if (name !== item.name) {
-        setAlert(true);
-        setName(item.name);
-      }
-    
+    if (name !== item.name) {
+      setAlert(true);
+      setName(item.name);
+    }
 
-    
     cartcxt.addProduct({ ...item, quantity: 1 });
   };
 
   return (
     <div>
-      {productsArr.map((item) => {
-        
-        return (
-          // <Container fluid="md" className={classes.container}>
-          <>
-            <Figure>
-              <Figure.Caption className={classes.caption}>
-                <h3>{item.name}</h3>
-              </Figure.Caption>
-
-              <div className={classes.container}>
-                <Figure.Image
-                  width={250}
-                  height={250}
-                  alt="250x250"
-                  src={item.imageUrl}
-                />
-              </div>
-              <Figure.Caption>
-                <span className={classes.price}>$ {item.price}</span>
-                <span>
-                  <Button
-                    variant="info"
+      <Container
+        className="justify-content-xs-center"
+        style={{ background: "red", marginTop: "10rem" }}
+      >
+        <div>
+          <Row
+          // className="justify-content-xs-center"
+          // xs={{ span: 2, offset: 1 }}
+          >
+            {productsArr.map((item) => {
+              return (
+                <Col
+                  className="justify-content-xs-center"
+                  key={item.id}
+                  sm={{ span: 3, offset: 2 }}
+                  xs={{ span: 4, offset: 3 }}
+                >
+                  <h3
                     style={{
-                      color: "white",
-                      fontWeight: "bold",
-                      borderRadius: "0px",
-                      marginTop: "10px",
+                      textAlign: "center",
+                      marginBottom: "3rem",
+                      marginTop: "1rem",
                     }}
-                    onClick={() => addItemTocart(item)}
                   >
-                    ADD TO CART
-                  </Button>
-                </span>
-              </Figure.Caption>
-            </Figure>
+                    {item.name}
+                  </h3>
 
-            {alert && <AlertMessage name={name} />}
-          </>
-        );
-      })}
+                  {/* <div className={classes.container}> */}
+                  <img
+                    width={200}
+                    height={200}
+                    alt="200x200"
+                    src={item.imageUrl}
+                  />
+                  {/* </div> */}
+                  <div
+                    style={{
+                      display: "flex",
+                      justifyContent: "space-between",
+                      // marginTop: "1px",
+                    }}
+                  >
+                    <span
+                      style={{
+                        color: "black",
+                        marginTop: ".5rem",
+                        // PaddingRight: "2px",
+                      }}
+                    >
+                      $ {item.price}
+                    </span>
+                    <span>
+                      <Button
+                        variant="info"
+                        style={{
+                          color: "white",
+                          fontWeight: "bold",
+                          borderRadius: "0px",
+                          marginTop: "10px",
+                          marginBottom: "10px",
+                          // marginLeft: "30px",
+                        }}
+                        onClick={() => addItemTocart(item)}
+                      >
+                        ADD TO CART
+                      </Button>
+                    </span>
+                  </div>
+
+                  {alert && <AlertMessage name={name} />}
+                </Col>
+              );
+            })}
+          </Row>
+        </div>
+      </Container>
     </div>
   );
 };
 
 export default ProductList;
+
+// {/* <Container fluid style={{ background: "red", marginTop: "10rem" }}>
+//   <div>
+//     <Row className="justify-content-xs-center" xs={{ span: 2, offset: 1 }}>
+//       {images.map((image) => (
+//         <Col
+//           // className="justify-content-xm-center"
+//           key={image.id}
+//           sm={{ span: 4, offset: 2 }}
+//           xs={{ span: 4, offset: 3 }}
+//         >
+
+//           <img src={image.src} width={150} height={156} alt="150x156" />
+//           <div
+//             style={{
+//               marginTop: "5px",
+//               marginBottom: "15px",
+//             }}
+//           >
+//             <span style={{ paddingRight: "12px" }}>Id :{image.id}</span>
+//             <Button> Add to Cart</Button>
+//           </div>
+//         </Col>
+//       ))} */}
+//     </Row>
+//   </div>
+// </Container>;
