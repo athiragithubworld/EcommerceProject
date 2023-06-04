4b104452054e323b6f029c4301b1e8577a34b0ba
athiragithubworld <athirahrms@gmail.com>
2023-06-04 22:52:51 +0530

    App js - some updates

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import React, { useState } from "react";
-
+import React, { useState, useContext, useEffect } from "react";
 import Ecommerce from "./Components/Ecommerce";
-import { Button } from "react-bootstrap";
 import Cart from "./Components/Cart/Cart";
-import CartContext from "./Components/store/CartContext";
 import CartProvider from "./Components/store/CartProvider";
 import About from "./Components/Pages/About";
 import ProductStore from "./Components/Pages/ProductStore";
-// import { BrowserRouter } from "react-router-dom";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
 import Home from "./Components/Pages/Home";
 import FooterNavBar from "./Components/UI/FooterNavBar";
 import HomeHeader from "./Components/UI/HomeHeader";
 import ContactUs from "./Components/Pages/ContactUs";
 import ProductDetails from "./Components/Pages/ProductDetails";
-
+import LoginPage from "./Components/Pages/LoginPage";
+import AuthProvider from "./Components/store/AuthProvider";
+import AuthContext from "./Components/store/AuthContext";
 // import ProductList from "./Components/ProductList";
 
 function App() {
+  const authcntx = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const logined = authcntx.isLoggedIn;
+
+  console.log("logined", logined);
+
   const [openCart, setOpenCart] = useState(false);
 
   const clickOpenCart = () => {
@@ -28,22 +33,37 @@ function App() {
     setOpenCart(false);
   };
 
+  useEffect(() => {
+    if (authcntx.isLoggedIn) {
+      console.log("ansss");
+      navigate("/store");
+    }
+  }, [authcntx.isLoggedIn]);
+
   return (
-    <CartProvider>
-      {/* <BrowserRouter> */}
+    <>
       <Ecommerce />
       <HomeHeader></HomeHeader>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/" element={<ProductStore onClicks={clickOpenCart} />} />
+
+        {authcntx.isLoggedIn && (
+          <Route
+            path="/store"
+            element={<ProductStore onClicks={clickOpenCart} />}
+          />
+        )}
+        {!authcntx.isLoggedIn && (
+          <Route path="/store" element={<Navigate to="/login" />} />
+        )}
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/contactus" element={<ContactUs />} />
         <Route path="/productDetails/:id" element={<ProductDetails />} />
       </Routes>
       <FooterNavBar></FooterNavBar>
-      {openCart && <Cart onClose={closeCart}></Cart>}
-      {/* </BrowserRouter> */}
-    </CartProvider>
+      {authcntx.isLoggedIn && openCart && <Cart onClose={closeCart}></Cart>}
+    </>
   );
 }
 
