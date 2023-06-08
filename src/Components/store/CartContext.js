081b3421ce6ae10068e68938bb76a8ba6f9295cf
athiragithubworld <athirahrms@gmail.com>
2023-06-08 22:43:44 +0530

    CartContext js file

--- src/Components/store/CartContext.js
+++ src/Components/store/CartContext.js
@@ -1,15 +1,13 @@
 import React from "react";
 
 const CartContext = React.createContext({
-    itemList:[],
-    quantity: 0,
-    totalAmount:0,
-    purchaseHandler: ()=>{},
-    addProduct:(product) => {},
-    removeProduct:(product) => {},
-    // alert:''
-    
-
-})
+  itemList: [],
+  quantity: 0,
+  totalAmount: 0,
+  purchaseHandler: () => {},
+  addProduct: (product) => {},
+  removeProduct: (product) => {},
+  // alert:''
+});
 
 export default CartContext;
