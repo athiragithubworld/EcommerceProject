cfdf01cea0a9ed4ae87285a6c2a31b559cadfa60
athiragithubworld <athirahrms@gmail.com>
2023-06-08 22:42:47 +0530

    AuthProvider js (Get Emailid without symbol)

--- src/Components/store/AuthProvider.js
+++ src/Components/store/AuthProvider.js
@@ -2,19 +2,23 @@ import React, { useState } from "react";
 import AuthContext from "./AuthContext";
 
 const AuthProvider = (props) => {
-  const intialToken = localStorage.getItem("token");
+  const intialToken = localStorage.getItem("emailobj");
+
   const [token, setToken] = useState(intialToken);
+  const [email, setEmail] = useState("");
 
   const userLoggedIn = !!token;
 
   const logoutHandler = () => {
     setToken(null);
-    localStorage.removeItem("token");
+    localStorage.removeItem("emailobj");
   };
 
-  const loginHandler = (token) => {
+  const loginHandler = (token, email) => {
     setToken(token);
-    localStorage.setItem("token", token);
+    setEmail(email);
+    const emailobj = { token: token, email: email };
+    localStorage.setItem("emailobj", JSON.stringify(emailobj));
     // setTimeout(() => {
     //   logoutHandler();
     // }, 30000);
@@ -22,11 +26,12 @@ const AuthProvider = (props) => {
 
   const contxtvalue = {
     token: token,
+
     isLoggedIn: userLoggedIn,
     login: loginHandler,
     logout: logoutHandler,
   };
-  console.log("aa", contxtvalue);
+
   return (
     <AuthContext.Provider value={contxtvalue}>
       {props.children}
